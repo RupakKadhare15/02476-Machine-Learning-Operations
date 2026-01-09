c78f99e9e663ea9862cf80466e37f229caab2ac3
Florian Küfner <florian.kuefner@rwth-aachen.de>
2026-01-09 11:22:43 +0100

    another try to satisfy ruff..

--- src/toxic_comments/train.py
+++ src/toxic_comments/train.py
@@ -2,8 +2,8 @@ from toxic_comments.model import Model
 from toxic_comments.data import MyDataset
 
 def train():
-    # dataset = MyDataset("data/raw")
-    # model = Model()
+    _ = MyDataset("data/raw") # _ => dataset
+    _ = Model() # _ => model
     # add rest of your training code here
     pass
 
