f58be2419cea52b8b35af7dc54a13326cd34735a
Florian Küfner <florian.kuefner@rwth-aachen.de>
2026-01-09 11:58:05 +0100

    Linting fixes, ignoring test folder for now.

--- src/toxic_comments/data.py
+++ src/toxic_comments/data.py
@@ -1,33 +1,37 @@
-import requests
 import shutil
 from pathlib import Path
 
+import requests
+
 # --- CONFIG ---
-DRIVE_ID = "1czsN8ebcoAkwAhs6rKdw3Enz0oBzdzTP"
-DATA_PATH = Path("data")
+DRIVE_ID = '1czsN8ebcoAkwAhs6rKdw3Enz0oBzdzTP'
+DATA_PATH = Path('data')
+
 
 def main():
     # This creates the directory
     DATA_PATH.mkdir(parents=True, exist_ok=True)
-    zip_path = DATA_PATH / "data.zip"
+    zip_path = DATA_PATH / 'data.zip'
 
-    print("Downloading...")
-    url = "https://docs.google.com/uc?export=download"
+    print('Downloading...')
+    url = 'https://docs.google.com/uc?export=download'
     session = requests.Session()
     response = session.get(url, params={'id': DRIVE_ID}, stream=True)
-    
+
     # Handle Google's virus warning token
     token = next((v for k, v in response.cookies.items() if k.startswith('download_warning')), None)
     if token:
         response = session.get(url, params={'id': DRIVE_ID, 'confirm': token}, stream=True)
 
-    with open(zip_path, "wb") as f:
+    with open(zip_path, 'wb') as f:
         for chunk in response.iter_content(32768):
-            if chunk: f.write(chunk)
+            if chunk:
+                f.write(chunk)
 
-    print("Extracting...")
+    print('Extracting...')
     shutil.unpack_archive(zip_path, DATA_PATH)
-    print("Done.")
+    print('Done.')
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
