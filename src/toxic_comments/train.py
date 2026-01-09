e811e3d30afa55a2dc3c2e35ec017440159bcadb
Florian Küfner <florian.kuefner@rwth-aachen.de>
2026-01-09 11:19:28 +0100

    manually removed unused variables to resolve ruff error

--- src/toxic_comments/train.py
+++ src/toxic_comments/train.py
@@ -2,9 +2,10 @@ from toxic_comments.model import Model
 from toxic_comments.data import MyDataset
 
 def train():
-    dataset = MyDataset("data/raw")
-    model = Model()
+    # dataset = MyDataset("data/raw")
+    # model = Model()
     # add rest of your training code here
+    pass
 
 if __name__ == "__main__":
     train()
