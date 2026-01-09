300365cb575809da1e1062fa23e09d1cee102443
Florian Küfner <florian.kuefner@rwth-aachen.de>
2026-01-09 10:06:07 +0100

    Updated ruff config and ran 'format' on /src.

--- src/toxic_comments/train.py
+++ src/toxic_comments/train.py
@@ -1,10 +1,12 @@
 from toxic_comments.model import Model
 from toxic_comments.data import MyDataset
 
+
 def train():
-    dataset = MyDataset("data/raw")
+    dataset = MyDataset('data/raw')
     model = Model()
     # add rest of your training code here
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     train()
